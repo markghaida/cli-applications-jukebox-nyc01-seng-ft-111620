cdc01a095f16efcfcaaf77a000f46d3f047ac91c
Mark G <markghaida@gmail.com>
2020-11-12 22:55:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,7 +20,7 @@ def play(songs)
   user_input = gets.strip
     if user_input.to_i <= songs.length && user_input.to_i >= 1
       puts "Playing #{songs[user_input.to_i - 1]}"
-    elsif songs.include? user_input
+    elsif songs.include? (user_input)
     #binding.pry
       # puts "Playing #{song}"
     # else
