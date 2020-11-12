e7ff9f6d4d0f79a8319e7661ccee71aa531156a5
Mark G <markghaida@gmail.com>
2020-11-12 22:41:19 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -17,7 +17,6 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  puts "Please enter a song name or number:"
   user_input = gets.strip
   #binding.pry
   songs.each_with_index do |song, index|
